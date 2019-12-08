feb23d2c9370aa5693278a9e2e2ad9eb2d4d6f5b
Nastussha <v-n-09@yandex.ru>
2019-12-09 00:59:34 +0300

    #2 commit. Task 70 PageFactory pattern. Added takeScreenshot method

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import pageFactory.LoginPopup;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
@@ -27,14 +32,25 @@ public class LoginTest {
     }
 
     @Test
-    public void login(){
+    public void login() throws Exception {
         loginPopup = new LoginPopup(driver);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        takeScreenshot();
         loginPopup.openLoginPopup();
         loginPopup.setUserName("seleniumtests@tut.by");
         loginPopup.setPassword("123456789zxcvbn");
         loginPopup.clickLoginButton();
 
         Assertions.assertTrue(loginPopup.getLoggedInResult(), "User is not logged in");
+    }
+
+
+    public void takeScreenshot() {
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileHandler.copy(src, new File("./target/screenshot.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
